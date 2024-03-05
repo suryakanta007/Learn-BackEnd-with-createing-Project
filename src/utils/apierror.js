f11c9115fdeb0e6683f18b37fb23126b0731c724
suryakanta007 <sahusuryakanta073@gmail.com>
2024-03-05 06:23:40 +0530

    fixing a speling in apierror utils

--- src/utils/apierror.js
+++ src/utils/apierror.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statuscode,
         message="Something Went Wrong",
         errors=[],
-        statck=""
+        stack=""
     ){
         super(message),
         this.statuscode=statuscode
@@ -12,8 +12,8 @@ class ApiError extends Error{
         this.success = false
         this.errors = errors
         
-        if (statck) {
-            this.stack=statck;
+        if (stack) {
+            this.stack=stack;
         }else{
             Error.captureStackTrace(this,this.constructor)
         }
